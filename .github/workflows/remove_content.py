2a8ffcc27c0d7f2d6fc8e42306919eb01a71a1f7
Tina-062006 <90503856+Tina-062006@users.noreply.github.com>
2024-06-12 14:29:34 +0800

    Update remove_content.py

--- .github/workflows/remove_content.py
+++ .github/workflows/remove_content.py
@@ -1,9 +1,9 @@
 # remove_content.py
 marker = "##########VOD#########"
-input_file = "input.txt"
-output_file = "output.txt"
+input_file = "https://raw.githubusercontent.com/Tina-062006/java/main/Master.m3u"
+output_file = "https://raw.githubusercontent.com/Tina-062006/java/main/Master.m3u"
 
-with open(input_file, "r") as file:
+with open(input_file, "w") as file:
     lines = file.readlines()
 
 with open(output_file, "w") as file:
