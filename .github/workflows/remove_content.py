d116a2471a0adc4d04b02e5d72116b9854891dee
Tina-062006 <90503856+Tina-062006@users.noreply.github.com>
2024-06-12 14:36:37 +0800

    Update remove_content.py

--- .github/workflows/remove_content.py
+++ .github/workflows/remove_content.py
@@ -3,7 +3,7 @@ marker = "##########VOD#########"
 input_file = "https://raw.githubusercontent.com/Tina-062006/java/main/Master.m3u"
 output_file = "https://raw.githubusercontent.com/Tina-062006/java/main/Master.m3u"
 
-with open(input_file, "w") as file:
+with open(input_file, "r") as file:
     lines = file.readlines()
 
 with open(output_file, "w") as file:
